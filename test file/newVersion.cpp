dd28f83ce4cd4a2d993c153ac4fc72181bf50f51
John <jle148@txstate.edu>
2022-11-06 15:37:32 -0600

    new version update

--- test file/newVersion.cpp
+++ test file/newVersion.cpp
@@ -10,47 +10,47 @@
 
 using namespace std;
 
-bool gameover(bool &);
-int miniMax(char **, int , char, int, int, int);
-void player_turn();
-void display_board();
-int evaluate1(char **);
-vector<char [9]> moveGen(char **, char);
-char opposite(char);
-int determineEval(char);
-
 //structure used in the minimax function
 struct Pathway {
     int value;
-    vector<char **> PATH;
-}
+    vector<char *> PATH;
+} path;
 
-//structure containing value of the lowest node and the path to get there
-Pathway path;
+bool gameover(char *, bool &);
+Pathway miniMax(char *, int , char, int, int);
+void player_turn();
+void display_board(char *);
+int evaluate1(char *, char);
+int evaluate2(char *, char);
+int evaluate3(char *, char);
+int evaluate4(char *, char);
+vector<char *> moveGen(char *, char);
+char opposite(char);
+int determineEval(char);
+
+
+
 //best path calculated so far
-vector<char **> bestPath;
+vector<char *> bestPath;
 
 int main()
 {
     //wanted to minimize global variables
     //some of these may go away depending on how the minimax algo works
-    char board[3][3];
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            board[i][j] = 'A';
+    char *board = new char[9];
+    for (int i = 0; i < 9; i++)
+        board[i] = 'A'+i;
     int choice;
     int row,column;
     char turn = 'X';
     char player = 'X';
-    char opponent = 'O';
     bool draw = false;
 
     cout<<"TicTacToe"<<endl;
 
-    while(!gameover()){
-        display_board();
-        player_turn();
-        gameover();
+    while(!gameover(board, draw)){
+        display_board(board);
+        miniMax(board, 0, 'X', -(numeric_limits<int>::max()), numeric_limits<int>::max());
     }
     if(turn == 'O' && draw == false){
         cout<<"Player 'X' has won the game";
@@ -63,50 +63,45 @@ int main()
 }
 
 //tweaked this one a little bit. i felt like gameover should be true when the game ended
-bool gameover(bool &draw) {
+bool gameover(char *board, bool &draw) {
     for(int i=0; i<3; i++) {
-        if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        if(board[i] == board[i+3] == board[i+6])
             return true;
-        if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+        if(board[(3*i)] == board[(3*i)+1] == board[(3*i)+2])
+            return true;
+        if(board[0] == board[4] == board[8] || board[2] == board[4] == board[6])
             return true;
     }
 
-    for(int i=0; i<3; i++)
-        for(int j=0; j<3; j++)
-            if(board[i][j] != 'X' && board[i][j] != 'O')
-                return false;
+    for(int i=0; i<9; i++)
+        if(board[i] != 'X' && board[i] != 'O')
+            return false;
 
     draw = true;
-    return true;        
+    return true;
 }
 
 //returns a vector that is the list of board positions in the next ply of the tree
-vector<char **> moveGen(char **pos, char plyr) {
-    vector<char **> moves;
+vector<char *> moveGen(char *board, char plyr) {
+    vector<char *> moves;
     if (plyr == 'X') {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 9; i++)
 		{
-			for (int j = 0; j < 3; j++) {
-                if (board[i][j] == 'A') {
-				board[i][j] = 'X';
+			 if (board[i] == 'A') {
+                board[i] = 'X';
 				moves.push_back(board);
-				board[i][j] = 'A';
-			}
-            }
-            
+				board[i] = 'A';
+			 }
 		}
     }
     else {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 9; i++)
 		{
-			for (int j = 0; j < 3; j++) {
-                if (board[i][j] == 'A') {
-				board[i][j] = 'O';
+			 if (board[i] == 'A') {
+                board[i] = 'O';
 				moves.push_back(board);
-				board[i][j] = 'A';
-			}
-            }
-            
+				board[i] = 'A';
+			 }
 		}
     }
     return moves;
@@ -125,26 +120,26 @@ char opposite(char plyr) {
     else return 'X';
 }
 
-void display_board(){
+void display_board(char *board){
     cout<<"PLAYER - 1 [X] PLAYER - 2 [O]"<<endl;
     cout<<"     |     |     "<<endl;
-    cout<<"  "<<board[0][0]<<"  | " <<board[0][1]<<"   |  "<<board[0][2]<<endl;
+    cout<<"  "<<board[0]<<"  | " <<board[1]<<"   |  "<<board[2]<<endl;
     cout<<"_____|_____|_____"<<endl;
     cout<<"     |     |     "<<endl;
-    cout<<"  "<<board[1][0]<<"  | " <<board[1][1]<<"   |  "<<board[1][2]<<endl;
+    cout<<"  "<<board[3]<<"  | " <<board[4]<<"   |  "<<board[5]<<endl;
     cout<<"_____|_____|_____"<<endl;
     cout<<"     |     |     "<<endl;
-    cout<<"  "<<board[2][0]<<"  | " <<board[2][1]<<"   |  "<<board[2][2]<<endl;
+    cout<<"  "<<board[6]<<"  | " <<board[7]<<"   |  "<<board[8]<<endl;
     cout<<"     |     |     "<<endl;
 }
 
 
-int evaluate1(char board[3][3]){
-    int maxi =0;
+int evaluate1(char *board, char player){
+    /*int maxi =0;
     int mini =0;
 
     for(int row =0; row<3; row++){
-        if(board[row][0]!= opponent && board[row][1]!= opponent && board[row][2]!= opponent){
+        if(board[row][0]!= opposite(player) && board[row][1]!= opposite(player) && board[row][2]!= opposite(player)){
             maxi++;
         }
         if(board[row][0]!= player && board[row][1]!= player && board[row][2]!= player){
@@ -152,20 +147,20 @@ int evaluate1(char board[3][3]){
         }
     }
     for(int col =0; col<3; col++){
-        if(board[0][col]!= opponent && board[1][col]!= opponent && board[2][col]!= opponent){
+        if(board[0][col]!= opposite(player) && board[1][col]!= opposite(player) && board[2][col]!= opposite(player)){
             maxi++;
         }
         if(board[0][col]!= player && board[1][col]!= player && board[2][col]!= player){
             mini++;
         }
     }
-    if(board[0][0]!= opponent && board[1][1]!= opponent && board[2][2]!= opponent){
+    if(board[0][0]!= opposite(player) && board[1][1]!= opposite(player) && board[2][2]!= opposite(player)){
         maxi++;
     }
     if(board[0][0]!= player && board[1][1]!= player && board[2][2]!= player){
         mini++;
     }
-    if(board[0][2]!= opponent && board[1][1]!= opponent && board[2][0]!= opponent){
+    if(board[0][2]!= opposite(player) && board[1][1]!= opposite(player) && board[2][0]!= opposite(player)){
         maxi++;
     }
     if(board[0][2]!= player && board[1][1]!= player && board[2][0]!= player){
@@ -173,11 +168,25 @@ int evaluate1(char board[3][3]){
     }
 
     return maxi-mini;
+    */
+    return 0;
+}
+
+int evaluate2(char *board, char player) {
+    return 0;
+}
+
+int evaluate3(char *board, char player) {
+    return 0;
+}
+
+int evaluate4(char *board, char player) {
+    return 0;
 }
 
 //returns a Pathway structure. The structure could probably use a better name
-Pathway miniMax(char **pos, int depth, char plyr, int useThr, int passThr) {
-    vector<char **> successors;
+Pathway miniMax(char *board, int depth, char plyr, int useThr, int passThr) {
+    vector<char *> successors;
     int resultSucc, newValue;
     int evalFunc = determineEval(plyr);
 
@@ -186,42 +195,42 @@ Pathway miniMax(char **pos, int depth, char plyr, int useThr, int passThr) {
     if (depth == deepEnough){
         switch (evalFunc) {
             case 1:
-                path.value = evaluate1(pos);
+                path.value = evaluate1(board, plyr);
                 return path;
             case 2:
-                path.value = evaluate(pos);
+                path.value = evaluate2(board, plyr);
                 return path;
             case 3:
-                path.value = evaluate3(pos);
+                path.value = evaluate3(board, plyr);
                 return path;
             case 4:
-                path.value = evaluate4(pos);
+                path.value = evaluate4(board, plyr);
                 return path;
         }
     }
     //step 2 in the psuedocode
     else {
-        successors = moveGen(pos, plyr);
+        successors = moveGen(board, plyr);
         //step 3 in the pseudocode
         if (successors.size() == 0) {
             switch (evalFunc) {
                 case 1:
-                    path.value = evaluate1(pos);
+                    path.value = evaluate1(board, plyr);
                     return path;
                 case 2:
-                    path.value = evaluate(pos);
+                    path.value = evaluate2(board, plyr);
                     return path;
                 case 3:
-                    path.value = evaluate3(pos);
+                    path.value = evaluate3(board, plyr);
                     return path;
                 case 4:
-                    path.value = evaluate4(pos);
+                    path.value = evaluate4(board, plyr);
                     return path;
             }
         }
         //step 4 in the pseudocode
-        for (char **succ : successors) {
-            resultSucc = miniMax(succ, depth + 1, opposite(plyr), -passThr, -useThr);
+        for (char *succ : successors) {
+            resultSucc = miniMax(succ, depth + 1, opposite(plyr), -passThr, -useThr).value;
             newValue = -resultSucc;
             if (newValue > passThr) {
                 passThr = resultSucc;
@@ -236,5 +245,5 @@ Pathway miniMax(char **pos, int depth, char plyr, int useThr, int passThr) {
         //step 5 in the pseudocode
         return path;
     }
-        
+
 }
